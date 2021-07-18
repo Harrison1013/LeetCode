2b5d7c0cd23bc84141d5d0e70a285f493dc44190
HAARP <2545782@gmail.com>
2021-07-18 21:25:16 +0800

    0718 Valid Mountain Array

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from Arrays.MaxConsecutiveOnes import Solution
+from Arrays.MountainArray import Solution
 
 if __name__ == '__main__':
-    num = [1, 1]
-    print(Solution().find_max_consecutive_ones(nums=num))
+    num = [2,1,2,3,5,7,9,10,12,14,15,16,18,14,13]
+    print(Solution().validMountainArray(arr=num))
